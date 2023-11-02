1703146a73a9c86bdc70c03d301a40cb1203c826
TueRask <100601369+TueRask@users.noreply.github.com>
2023-11-02 23:16:28 +0100

    Added UserService closes #5 Added random sample users to UserService Updated User class, added getters Added endpoint to get number of players Added test endpoint to get all users from UserService

--- src/main/java/Backend/API/UserController.java
+++ src/main/java/Backend/API/UserController.java
@@ -1,13 +1,23 @@
 package Backend.API;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class UserController {
+    private UserService userService;
+    @Autowired //Autowired constructor to add Service to the RestController
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/hello") // Tells Spring that this is a GET endpoint
     ResponseEntity<String> hello(@RequestParam int n) {
         if (n > 0) {
@@ -22,6 +32,18 @@ public class UserController {
         }
     }
 
+    //test path
+    @GetMapping("/users")
+    ResponseEntity<List<User>> Users() {
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i< userService.GetSize(); i++) {
+            users.add(userService.GetUser(i));
+        }
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+
+
     /*
     getUsersByRank(int min, int max, String region)
         list of users
@@ -32,7 +54,11 @@ public class UserController {
     getUserByScore(int score)
         index of user
 
-    getNumberOfPlayers()
+    getNumberOfPlayers() //called it size
         number of players
      */
+    @GetMapping("/size")
+    ResponseEntity<Integer> Size(){
+        return new ResponseEntity<>(userService.GetSize(), HttpStatus.OK);
+    }
 }
---
+++ src/main/java/Backend/API/UserService.java
@@ -0,0 +1,56 @@
+package Backend.API;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@Service //Service for interacting with the database to get user data
+public class UserService {
+    private List<User> users = new ArrayList<>();
+
+    private Random rand = new Random();
+
+    private String RandomName() {
+        List<String> nouns = Arrays.asList("Gamer", "Eagle", "Cobra", "Priest", "Pilot", "Ace", "Officer", "Commander", "Dragon", "Swan", "Dolphin", "Hawk", "Vulture", "Mole", "Toucan", "Lizard", "Moose", "Bamboo", "Robber", "Painter", "Sheriff", "Judge", "Cook", "Baron", "King", "Lord", "Queen", "Emperor", "President", "Astronomer", "Astronaut", "Expert");
+        List<String> adjectives = Arrays.asList("Mystic", "Elite", "Disguising", "Mighty", "Big", "Tiny", "Aerobatic", "Lanky", "Fearful", "Shocking", "Striking", "Practical", "Unlucky", "Sweaty", "Floppy", "Pensive", "Steady", "Icky", "Unlawful", "Abnormal", "Friendly", "Receptive", "Maternal", "Juicy", "Grotesque", "Gimmicky");
+        int noun = rand.nextInt(nouns.size());
+        int adjective = rand.nextInt(adjectives.size());
+        int number = rand.nextInt(100);
+
+        return adjectives.get(adjective) + nouns.get(noun) + number;
+    }
+
+    private int RandomScore(int bound) {
+        return rand.nextInt(bound);
+    }
+
+    private String RandomRegion() {
+        List<String> regions = Arrays.asList("EU", "NA", "AS", "SA");
+        return regions.get(rand.nextInt(regions.size()));
+    }
+
+    private String RandomCountry() {
+        List<String> countries = Arrays.asList("DK", "SE", "GE", "UK", "US", "RU", "NO", "JP", "CH");
+        return countries.get(rand.nextInt(countries.size()));
+    }
+
+    public UserService() {
+        for (int i = 0; i<100; i++) {
+            users.add(new User(i, RandomName(), RandomScore(1000), RandomCountry(), RandomRegion()));
+        }
+        //need to sort users by score primary with timestamp as tiebreaker
+        //need a custom comparator
+    }
+
+    public int GetSize() {
+        return users.size();
+    }
+
+    public User GetUser(int index) {
+        return users.get(index);
+    }
+
+}
