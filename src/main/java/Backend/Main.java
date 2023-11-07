ffb4a8622cf39dde53c19527ec169d4148f70a75
Martin <Wolfguard@mail.com>
2023-11-07 11:11:33 +0100

    Added database connection, necessary dependencies and a little test.

--- src/main/java/Backend/Main.java
+++ src/main/java/Backend/Main.java
@@ -2,14 +2,19 @@ package Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import redis.clients.jedis.Jedis;
 
 @SpringBootApplication // Tells Spring that this is the main class
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+
+        // Connect to Redis Cloud database
+        Jedis jedis = new Jedis("redis-12618.c304.europe-west1-2.gce.cloud.redislabs.com", 12618);
+        jedis.auth("MdgWuJDGsrEQiRjP8rNawQNQ9Cls2Qp9");
+
+        // Print out the top 6 players (test)
+        jedis.zrevrange("playersByPoints", 0, 5).forEach(System.out::println);
     }
-
-
-
 }
