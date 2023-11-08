79c936f6157fdd1e7f223136b08d8703babe5b45
Emilsp3 <115782475+Emilsp3@users.noreply.github.com>
2023-11-08 12:36:53 +0100

    remove unused code

--- src/main/java/Backend/Main.java
+++ src/main/java/Backend/Main.java
@@ -1,21 +1,13 @@
 package Backend;
 
-import Backend.API.DBservice;
-import Backend.API.DatabaseService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import redis.clients.jedis.Jedis;
+
 
 @SpringBootApplication // Tells Spring that this is the main class
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-
-        // Connect to Redis Cloud database
-        Jedis jedis = DatabaseService.getJedis();
-        jedis.auth("MdgWuJDGsrEQiRjP8rNawQNQ9Cls2Qp9");
-
-
     }
 }
