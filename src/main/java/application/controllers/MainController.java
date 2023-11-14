08d1668c97ed03e89ced8d28f566c82387f9921f
Alexander <alzs21@student.aau.dk>
2023-11-14 13:53:44 +0100

    test

--- src/main/java/application/controllers/MainController.java
+++ src/main/java/application/controllers/MainController.java
@@ -26,7 +26,7 @@ public class MainController {
     ResponseEntity<List<Tuple>> Users(@RequestParam int min, int max) {
         return new ResponseEntity<>(databaseService.getPlayersByPoints(min, max), HttpStatus.OK);
     }
-
+//remove this
     /*
     getUsersByRank(int min, int max, String region)
         list of users
