f90ab4b0e67badc7e9ae5e465437f274967f1d86
MagnusHoese <116256735+MagnusHoese@users.noreply.github.com>
2023-11-22 11:52:45 +0100

    Merge fix

--- leaderboardServer/src/main/java/application/services/DatabaseService.java
+++ leaderboardServer/src/main/java/application/services/DatabaseService.java
@@ -35,9 +35,10 @@ public class DatabaseService {
     private final String CLOUD_SERVER_PASSWORD = "MdgWuJDGsrEQiRjP8rNawQNQ9Cls2Qp9";
 
     public DatabaseService() {
-        //this.jedisPool = new JedisPool(AAU_SERVER_IP, AAU_PORT, "default", AAU_SERVER_PASSWORD);
-        this.jedisPool = new JedisPool(CLOUD_SERVER_IP, CLOUD_PORT, "default", CLOUD_SERVER_PASSWORD);
-
+        //this.jedisPool = new JedisPool("130.225.39.42", 6379, "default", "tJ1Y37fGm5c2A2m6jCE0");
+        this.jedisPool = new JedisPool("redis-12618.c304.europe-west1-2.gce.cloud.redislabs.com", 12618, "default", "MdgWuJDGsrEQiRjP8rNawQNQ9Cls2Qp9");
+        //this.jedisPool = new JedisPool("localhost", 6379);
+        //new DatabasePopulator(jedisPool);
     }
 
     public Jedis getJedisConnection() {
