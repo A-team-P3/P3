d7d69cbd0c23059209ef19e77e33fad317b75d7d
Fred_Dead_Redemption <frederikdau@gmail.com>
2023-12-08 10:34:25 +0100

    loading svg

--- leaderboardServer/Svelte/src/App.svelte
+++ leaderboardServer/Svelte/src/App.svelte
@@ -246,7 +246,9 @@
     </div>
   </nav>
   <!--TABLE-->
-  {#if loading === false}
+  {#if loading}
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><path fill="#232334" stroke="%235989B1" stroke-width="15" transform-origin="center" d="m148 84.7 13.8-8-10-17.3-13.8 8a50 50 0 0 0-27.4-15.9v-16h-20v16A50 50 0 0 0 63 67.4l-13.8-8-10 17.3 13.8 8a50 50 0 0 0 0 31.7l-13.8 8 10 17.3 13.8-8a50 50 0 0 0 27.5 15.9v16h20v-16a50 50 0 0 0 27.4-15.9l13.8 8 10-17.3-13.8-8a50 50 0 0 0 0-31.7Zm-47.5 50.8a35 35 0 1 1 0-70 35 35 0 0 1 0 70Z"><animateTransform type="rotate" attributeName="transform" calcMode="spline" dur="2" values="0;120" keyTimes="0;1" keySplines="0 0 1 1" repeatCount="indefinite"></animateTransform></path></svg>
+  {:else}
     <div id="leaderboard-body" bind:this={table}>
       {#each players as player}
         <li>
@@ -265,8 +267,6 @@
         </li>
       {/each}
     </div>
-  {:else}
-    <h1 style="display: flex; align-self: center; justify-self: center;">LOADING MAKKER</h1>
   {/if}
 </div>
 
